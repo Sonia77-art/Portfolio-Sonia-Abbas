9bf78e5be50e667c7c3f5485d482f477308a2d74
Sonia77-art <soniasobby@gmail.com>
2022-11-30 13:23:00 +0200

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,23 @@
 import "../App.css";
-import { RiHome7Line } from "react-icons/ri";
+import { ImLinkedin } from "react-icons/im";
+import { GrFacebook } from "react-icons/gr";
+import { BsGithub } from "react-icons/bs";
+import { SiInstagram } from "react-icons/si";
 
 export default function Home() {
   return (
     <div>
-      <a href="#" Home>
-        <RiHome7Line />
+      <a href="https://linkedin.com" target="_blank">
+        <ImLinkedin />
+      </a>
+      <a href="https://facebook.com" target="_blank">
+        <GrFacebook />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <BsGithub />
+      </a>
+      <a href="https://instagram.com" target="_blank">
+        <SiInstagram />
       </a>
     </div>
   );
