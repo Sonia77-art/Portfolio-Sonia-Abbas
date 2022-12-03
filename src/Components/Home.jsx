261aa37cc0bac4e9b54d00012618460461863c21
Sonia77-art <soniasobby@gmail.com>
2022-12-03 16:07:41 +0200

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,18 +7,22 @@ import { SiInstagram } from "react-icons/si";
 export default function Home() {
   return (
     <div className="Homi">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a classname="Link" href="https://linkedin.com" target="_blank">
         <ImLinkedin />
       </a>
       <br />
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="face" href="https://facebook.com" target="_blank">
         <GrFacebook />
       </a>
       <br />
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="Git" href="https://github.com" target="_blank">
         <BsGithub />
       </a>
       <br />
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="Insta" href="https://instagram.com" target="_blank">
         <SiInstagram />
       </a>
