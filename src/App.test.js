3881df56d458ee25eac0b078d423b2bc7b9055a7
Sonia77-art <soniasobby@gmail.com>
2022-12-03 15:30:51 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Sonia Abbas/i);
+  const linkElement = screen.getByText(/sonia abbas/i);
   expect(linkElement).toBeInTheDocument();
 });
