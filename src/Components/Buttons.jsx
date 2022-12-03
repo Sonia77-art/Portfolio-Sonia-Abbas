46b96bb019d1c462a2503030d1976de4adffe90e
Sonia77-art <soniasobby@gmail.com>
2022-12-03 17:12:54 +0200

    test

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,10 +2,12 @@ export default function Buttons() {
   return (
     <div>
       <button>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">Download Resume</a>
       </button>
 
       <button>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a href="#">Let's Talk More</a>
       </button>
     </div>
